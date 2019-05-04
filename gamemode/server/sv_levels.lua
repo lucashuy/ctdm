12759d76fb9c0a9729194533f359cf8ec230e23d
Lucas <contact@itslucas.win>
2019-05-03 21:32:23 -0500

    reading files from proper location now

--- gamemode/server/sv_levels.lua
+++ gamemode/server/sv_levels.lua
@@ -2,12 +2,13 @@ CTDM = CTDM or {}
 
 -- load level data into our level global table
 local function loadLevelData()
-    local file = file.Read("levels.json", "LUA")
+    -- TODO: move levels.json somewhere else, probably some config/content/data folder
+    local file = file.Read("gamemodes/ctdm/gamemode/levels.json", "GAME")
 
     if (file ~= nil) then
         CTDM.levels = util.JSONToTable(file)
     else
-        -- TODO: error on invalid level data
+        print("[CTDM] Invalid level data")
     end
 end
 loadLevelData()
